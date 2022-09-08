7c92965968c9704ed265c2c639a8dc31a785a65e
Roger S. Grading <roger@grading.no>
2022-09-08 16:19:13 +0200

    Receive messages instead

--- Program.cs
+++ Program.cs
@@ -26,48 +26,55 @@ public class Program
         // name of your Service Bus topic
         string queueName = "az204-queue";
 
-                // the client that owns the connection and can be used to create senders and receivers
-        ServiceBusClient client;
+        // the client that owns the connection and can be used to create senders and receivers
+        ServiceBusClient client = new ServiceBusClient(connectionString);
+        
+        // create a processor that we can use to process the messages
+        var processor = client.CreateProcessor(queueName, new ServiceBusProcessorOptions());
 
-        // the sender used to publish messages to the queue
-        ServiceBusSender sender;
-
-        // number of messages to be sent to the queue
-        const int numOfMessages = 3;
-
-                // Create the clients that we'll use for sending and processing messages.
-        client = new ServiceBusClient(connectionString);
-        sender = client.CreateSender(queueName);
-
-        // create a batch 
-        using ServiceBusMessageBatch messageBatch = await sender.CreateMessageBatchAsync();
-
-        for (int i = 1; i <= 3; i++)
+        try
         {
-            // try adding a message to the batch
-            if (!messageBatch.TryAddMessage(new ServiceBusMessage($"Message {i}")))
-            {
-                // if an exception occurs
-                throw new Exception($"Exception {i} has occurred.");
-            }
-        }
+            // add handler to process messages
+            processor.ProcessMessageAsync += MessageHandler;
 
-        try 
-        {
-            // Use the producer client to send the batch of messages to the Service Bus queue
-            await sender.SendMessagesAsync(messageBatch);
-            Console.WriteLine($"A batch of {numOfMessages} messages has been published to the queue.");
+            // add handler to process any errors
+            processor.ProcessErrorAsync += ErrorHandler;
+
+            // start processing 
+            await processor.StartProcessingAsync();
+
+            Console.WriteLine("Wait for a minute and then press any key to end the processing");
+            Console.ReadKey();
+
+            // stop processing 
+            Console.WriteLine("\nStopping the receiver...");
+            await processor.StopProcessingAsync();
+            Console.WriteLine("Stopped receiving messages");
         }
         finally
         {
             // Calling DisposeAsync on client types is required to ensure that network
             // resources and other unmanaged objects are properly cleaned up.
-            await sender.DisposeAsync();
+            await processor.DisposeAsync();
             await client.DisposeAsync();
         }
+    }
 
-        Console.WriteLine("Press any key to end the application");
-        Console.ReadKey();
+    // handle received messages
+    static async Task MessageHandler(ProcessMessageEventArgs args)
+    {
+        string body = args.Message.Body.ToString();
+        Console.WriteLine($"Received: {body}");
+
+        // complete the message. messages is deleted from the queue. 
+        await args.CompleteMessageAsync(args.Message);
+    }
+
+    // handle any errors when receiving messages
+    static Task ErrorHandler(ProcessErrorEventArgs args)
+    {
+        Console.WriteLine(args.Exception.ToString());
+        return Task.CompletedTask;
     }
 }
 
