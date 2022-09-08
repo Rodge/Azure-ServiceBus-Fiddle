e67b631440b2684631a856106a7c5f34f61f7c93
Roger S. Grading <roger@grading.no>
2022-09-08 16:03:57 +0200

    Read app settings

--- Program.cs
+++ Program.cs
@@ -1,3 +1,33 @@
-﻿using System.Threading.Tasks;    
+﻿using System.Threading.Tasks;  
 using Azure.Messaging.ServiceBus;
+using Microsoft.Extensions.Configuration;
 
+public class Program 
+{
+    public static async Task Main(string[] args)
+    {
+        // Build a config object, using env vars and JSON providers.
+        var environmentName = Environment.GetEnvironmentVariable("DOTNET_ENVIRONMENT");
+        IConfiguration config = new ConfigurationBuilder()
+            .AddJsonFile("appsettings.json")
+            .AddJsonFile($"appsettings.{environmentName}.json", true, true)
+            .AddEnvironmentVariables()
+            .Build();
+
+        // Get values from the config given their key and their target type.
+        ConnectionStrings connectionStrings = config.GetRequiredSection("ConnectionStrings").Get<ConnectionStrings>();
+
+        // Write the values to the console.
+        Console.WriteLine($"connectionStrings.ServiceBus.PrimaryConnectionString = {connectionStrings.ServiceBus.PrimaryConnectionString}\n");
+    }
+}
+
+public sealed class ConnectionStrings
+{
+    public ServiceBus ServiceBus { get; set; } = null!;
+}
+
+public sealed class ServiceBus
+{
+    public string PrimaryConnectionString { get; set; } = null!;
+}
